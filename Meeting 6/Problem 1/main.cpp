ebb080a3e08b7bcd247d1df2b9abe8b9ec6b3c27
adityaevanrhm <adityaevanrhm@gmail.com>
2024-10-14 17:04:05 +0700

    Update main.cpp

--- Meeting 6/Problem 1/main.cpp
+++ Meeting 6/Problem 1/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+struct fullname {
+    string firstname;
+    string lastname;
+};
+struct StudentRec {
+    fullname name;
+    string idNum;
+    float gpa;
+};
+StudentRec theStudent[10];
+
+int main() {
+    int n;
+    cout<<"Masukkan banyaknya mahasiswa : ";
+    cin>>n;
+    cout<<"Data mahasiswa"<<endl;
+for (int i=0;i<n;i++) {
+    cout<<"Nama depan : ";
+    cin>>theStudent[i].name.firstname;
+    cout<<"Nama belakang : ";
+    cin>>theStudent[i].name.lastname;
+    cout<<"Nim : ";
+    cin>>theStudent[i].idNum;
+    cout<<"IPK : ";
+    cin>>theStudent[i].gpa;
+}
+    cout<<endl;
+    cout<<"Data mahasiswa"<<endl;
+    for (int i=0;i<n;i++) {
+    cout<<theStudent[i].name.firstname<<endl;
+    cout<<theStudent[i].name.lastname<<endl;
+    cout<<theStudent[i].idNum<<endl;
+    cout<<theStudent[i].gpa<<endl;
+    }
+    system("Pause");
+return 0;
+}
