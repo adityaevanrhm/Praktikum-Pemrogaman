983c828fe85553c9ac17824e88d0cd5151196b70
adityaevanrhm <adityaevanrhm@gmail.com>
2024-11-03 22:48:14 +0700

    Update main.cpp

--- Meeting 5/Problem 1/main.cpp
+++ Meeting 5/Problem 1/main.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    std::vector<std::string> result;
+	vector<string> result;
 
-    // Loop from 1 to 100
-    for (int i = 1; i <= 100; ++i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            result.push_back("FizzBuzz");
-        } else if (i % 3 == 0) {
-            result.push_back("Fizz");
-        } else if (i % 5 == 0) {
-            result.push_back("Buzz");
-        } else {
-            result.push_back(std::to_string(i));
-        }
-    }
+	// looping from 1 to 100
+	for (int i = 1; i <= 100; i++) {
+		if (i%3==0 && i%5==0) {
+			result.push_back("FizzBuzz");
+		} else if (i%3==0) {
+			result.push_back("Fizz");
+		} else if (i%5==0) {
+			result.push_back("Buzz");
+		} else {
+			result.push_back(to_string(i));
+		}
+	}
+	
+	// Print the result
+	for(int i=0; i<(int)result.size(); i++)
+	{
+		cout<<result[i]<<' ';
+	}
+	cout<<'\n';
 
-    // Print the result
-    for (const auto& str : result) {
-        std::cout << str << ' ';
-    }
-
-    return 0;
+	return 0;
 }
