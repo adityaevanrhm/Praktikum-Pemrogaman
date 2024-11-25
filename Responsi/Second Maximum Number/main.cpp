ad54979a2e5156f640bd4b298fddeefcb4cc5d40
adityaevanrhm <adityaevanrhm@gmail.com>
2024-11-25 16:12:49 +0700

    Update main.cpp

--- Responsi/Second Maximum Number/main.cpp
+++ Responsi/Second Maximum Number/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int array[5] = {56,9,3,100,11};
+    int max = 0;
+    int max2 = 0;
+    for(int i = 0; i<5; i++){
+        if(array[i]>max){
+            max2 = max;
+            max = array[i];
+        }
+    }
+    cout << max << " is the maximum number and " << max2 << " is the second maximum number";
+    
+}
