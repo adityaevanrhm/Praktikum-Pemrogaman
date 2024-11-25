3dd4628924d7c9ef909c83c04644b798779ae894
adityaevanrhm <adityaevanrhm@gmail.com>
2024-11-25 16:12:23 +0700

    Update main.cpp

--- Responsi/Palindrome/main.cpp
+++ Responsi/Palindrome/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    string word = "racecar";
+    bool palindrome = true;
+    for(int i=0; i<7/2; i++){
+        if(word[i]==word[7-i-1]){
+            palindrome = true;
+        }
+        else palindrome = false;
+    }
+    if(!palindrome){
+        cout << "The word is not a palindrome";
+    }
+    else{cout << "The word is a palindrome";}
+    return 0;
+}
